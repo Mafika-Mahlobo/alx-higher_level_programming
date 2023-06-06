c442af88cfd40d61d62ca09cd9e6d1c70f13e73e
Mafika <rudolphmafika@gmail.com>
2023-06-05 18:31:29 -0700

    task 9 & 10 initial commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* check_cycle - checks if linked list has a cycle
+* @list: pointer to linked list
+* Return: Always 0
+*/
+
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
