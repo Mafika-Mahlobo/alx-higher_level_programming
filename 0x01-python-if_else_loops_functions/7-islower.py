5f84c2be6f61bd00a02c5d5db5868106a3ec6753
Mafika <rudolphmafika@gmail.com>
2023-06-07 00:06:08 -0700

    fixing errors task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >=  65 and ord(c) <= 90:
-        return False
+    if type(c) == str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            return True
+        else:
+            return False
     else:
-        return True
+        return False
