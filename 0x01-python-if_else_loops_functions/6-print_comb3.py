e8bceee3a8326f2ed8511e7616f8fb59aa1eec79
Mafika <rudolphmafika@gmail.com>
2023-06-06 23:24:37 -0700

    fixing errors task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i, 10):
-        if (((i * 10) + j)) < 99:
-            print("{:02},".format(((i * 10) + j)), end=" ")
-        else:
-            print("{:02}".format(((i * 10) + j)))
+        if not i == j:
+            if (((i * 10) + j)) < 89:
+                print("{:02},".format(((i * 10) + j)), end=" ")
+            else:
+                print("{:02}".format(((i * 10) + j)))
