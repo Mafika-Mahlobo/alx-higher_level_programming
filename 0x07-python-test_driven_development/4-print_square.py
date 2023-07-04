986580adb2bbebfe54b7d8dfa567b86eb5580894
Mafika <rudolphmafika@gmail.com>
2023-07-04 16:19:26 -0700

    tasks files

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# 4-print_square.py
+"""Defines a square-printing function."""
+
+
+def print_square(size):
+    """Print a square with the # character.
+
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0"""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
+
+        if __name__ == "__main__":
+            import doctest
+            
+            with open('test.txt', 'r') as file:
+                test_examples = file.read()
+
+            doctest.testmod(verbose=False)
