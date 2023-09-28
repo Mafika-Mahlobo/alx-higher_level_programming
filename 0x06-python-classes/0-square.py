b52550d3111e001c926326ec2bdf88b552d8fb6c
Mafika <rudolphmafika@gmail.com>
2023-09-27 20:18:51 -0700

    edited 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+
+"""
+Module with empty class
+"""
+
 class Square:
-    """A new class Square"""
+    """A new class Square
+        
+    Args:
+        No arguments
+
+    Returns:
+        None
+
+    """
     pass
