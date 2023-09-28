766e7a9f34222a786b7b7469cc114b29543a1732
Mafika <rudolphmafika@gmail.com>
2023-09-27 21:01:23 -0700

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+defining the size of a square
+"""
+
+class Square():
+
+    """
+    Args:
+        size(int): the size of the square
+
+    Returns:
+        No return
+
+    """
+
+    def __init__(self, size):
+        self.__size = size
