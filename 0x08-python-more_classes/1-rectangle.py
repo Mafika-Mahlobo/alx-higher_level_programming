97712a3300a83949e268befe16621494fbaa8356
Mafika <rudolphmafika@gmail.com>
2023-10-02 10:25:55 -0700

    fixed error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,14 +28,18 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
+    @property
     def width(self):
         return self.__width
 
+    @width.setter
     def width(self, value):
         self.__width = value
 
+    @property
     def height(self):
         return self.__height
 
+    @height.setter
     def height(self, value):
         self.__height = value
