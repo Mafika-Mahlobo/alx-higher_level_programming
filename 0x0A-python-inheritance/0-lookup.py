bf6a4cc284be7c64b999106b818ea94deea18b66
Mafika <rudolphmafika@gmail.com>
2023-10-09 18:03:58 -0700

    initial commit, task0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Module containing a function that returns a list of availble attribute or/and methods
+of  passed object
+"""
+
+def lookup(obj):
+	"""
+	this function takes an object and return a list of attributes
+	and/or methoda of the object
+	"""
+
+	return dir(obj)
