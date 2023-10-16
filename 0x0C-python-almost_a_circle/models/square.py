8f568f1989695120497273a4420c50326648d0d8
Mafika <rudolphmafika@gmail.com>
2023-10-16 23:41:13 +0000

    task12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,3 +27,27 @@ class Square(Rectangle):
     def size(self, size):
         """setter for size attribute"""
         self.width = size
+
+    def update(self, *args, **kwargs):
+        """ovweridig update method from parent class - rectangle"""
+
+        if args:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                if i == 1:
+                    self.size = args[i]
+                if i == 2:
+                    self.x = args[i]
+                if i == 3:
+                    self.y = args[i]
+
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
