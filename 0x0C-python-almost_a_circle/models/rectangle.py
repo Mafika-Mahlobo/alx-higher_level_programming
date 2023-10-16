03fd08f7f712b0ece799098a622bc377cc90201b
Mafika <rudolphmafika@gmail.com>
2023-10-16 21:17:52 +0000

    task8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -122,3 +122,18 @@ class Rectangle(Base):
             f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
             f"- {self.__width}/{self.__height}"
             )
+
+    def update(self, *args):
+        """Assigns no-keyword argument to class attributes"""
+
+        for i in range(len(args)):
+            if i == 0:
+                self.id = args[i]
+            elif i == 1:
+                self.__width = args[i]
+            elif i == 2:
+                self.__height = args[i]
+            elif i == 3:
+                self.__x = args[i]
+            elif i == 4:
+                self.__y = args[i]
